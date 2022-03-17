2870414e1bda2c034e6567a7c06159f8858f741d
Luis <luismhernandez@gmail.com>
2022-03-16 23:56:18 -0400

    I am done!

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b) {
+    return a+b
+}
+function subtract(a,b) {
+    return a-b
+}
+function multiply(a,b) {
+    return a*b
+}
+function divide(a,b) {
+    return a/b
+}
+
+add(50,25)
+subtract(50,25)
+multiply(50,25)
+divide(50,25)
+
+function increment(n) {
+    return n+1
+}
+
+function decrement(n) {
+    return n-1
+}
+
+function makeInt(n) {
+    return parseInt(n,10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
